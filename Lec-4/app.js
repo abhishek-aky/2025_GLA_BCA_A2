8ce4575880e3ce17f6d9635a4cfc9892edb2d409
Abhishek Kumar <er.abhirishab@gmail.com>
2026-01-19 23:39:38 +0530

    Lec-4

---
+++ Lec-4/app.js
@@ -0,0 +1,59 @@
+// function jodna(){ //
+//     let n1 = 10;
+//     let n2 = 20;
+//     // console.log(n1+n2);
+//     return n1+n2;
+// }
+
+// console.log(jodna);
+// console.log(jodna());
+// let res = jodna();
+// console.log(res);
+
+// function multi(num1, num2, num3){ // NaN
+//     let n1 = num1;
+//     let n2 = num2;
+//     return n1*n2*num3;
+// }
+// console.log(multi(10, 20));
+
+// function multi(num1, num2, num3=6){
+//     let n1 = num1;
+//     let n2 = num2;
+//     let n3 = num3;
+//     return n1*n2*n3;
+// }
+// console.log(multi(10, 20));
+// console.log(multi(10,20, 3));
+
+
+// const res = function multi(){
+//     let n1 = 10;
+//     let n2 = 20;
+//     return n1*n2;
+// }
+// console.log(res);
+// // console.log(multi(10, 20));
+// var resu = ()=>{
+//     let num1 = 45;
+//     let num2 = 20;
+//     return num1+num2;
+// }
+// console.log(resu());
+
+let person = {
+    naam:'Abhishek Kumar',
+    umar:24,
+    ug:'B.Tech',
+    Experience:'02'
+} 
+console.log(person.naam);
+// console.log(person.umar);
+console.log(person.naam.length);
+console.log(person.naam.toUpperCase());
+console.log(person.naam.toLowerCase());
+
+
+
+
+
